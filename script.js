1ede4b78e7afc20cb80237755b3767a5a14a3397
blueleafsoft <blueleafsoft@gmail.com>
2026-07-22 01:00:57 +0530

    code format

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const button = document.getElementById("countBtn");
 const countEl = document.getElementById("count");
 let count = 0;
 
-if (button && countEl) {
-  button.addEventListener("click", () => {
-    count += 1;
-    countEl.textContent = `Clicked ${count} time${count === 1 ? "" : "s"}`;
-  });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     fetch("strings.json")
         .then(response => {
