7e3a65eb3eeacfb6f8e3c98ba6f060307a63aee5
blueleafsoft <blueleafsoft@gmail.com>
2026-07-23 01:53:02 +0530

    Update customer page

---
+++ customer/script.js
@@ -0,0 +1,2 @@
+import { db } from "../firebase.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
--- strings.js
+++
@@ -1,6 +0,0 @@
-const STRINGS = {
-  APP_NAME: "GEAR",
-  CONTACT_EMAIL: "@gearapp.in",
-  PRIVACY_TITLE: "Privacy Policy",
-  TERMS_TITLE: "Terms & Conditions"
-};
